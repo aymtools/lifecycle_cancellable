5b5f37cd2916730125e8f3fcfce34f07ea16adf6
aym <anyaming0@gmail.com>
2024-05-14 22:00:37 +0800

    补充一个导出文件

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+* export dialog/dialog_ext.dart
+
 ## 0.0.1
 
 * The first version migration is completed
--- lib/an_lifecycle_cancellable.dart
+++ lib/an_lifecycle_cancellable.dart
@@ -2,4 +2,5 @@ library lifecycle_cancellable;
 
 export 'dialog/context_ext.dart';
 export 'dialog/navigator_ext.dart';
+export 'dialog/dialog_ext.dart';
 export 'lifecycle/lifecycle_ext.dart';

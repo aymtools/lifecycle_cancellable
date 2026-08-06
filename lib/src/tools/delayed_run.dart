e790e71e747087c851ae43dc9160c4959d2ff1ed
aymtools <anyaming0@gmail.com>
2026-08-06 14:38:56 +0000

    简化 runAfterNextFrameCallbackOrNextEventLoop

--- lib/src/tools/delayed_run.dart
+++ lib/src/tools/delayed_run.dart
@@ -1,25 +1,28 @@
-import 'package:flutter/scheduler.dart' show SchedulerPhase, Priority;
 import 'package:flutter/widgets.dart';
 
 void runAfterNextFrameCallbackOrNextEventLoop(void Function() callback) {
   final bindings = WidgetsBinding.instance;
 
-  switch (bindings.schedulerPhase) {
-    case SchedulerPhase.transientCallbacks:
-    case SchedulerPhase.midFrameMicrotasks:
-    case SchedulerPhase.persistentCallbacks:
-      bindings.addPostFrameCallback((_) => callback());
-      break;
-    case SchedulerPhase.postFrameCallbacks:
-    // Timer.run(callback);
-    // break;
-    case SchedulerPhase.idle:
-      // if (bindings.hasScheduledFrame) {
-      //   bindings.scheduleTask(callback, Priority.animation);
-      // } else {
-      //   Timer.run(callback);
-      // }
-      bindings.scheduleTask(callback, Priority.touch);
-      break;
-  }
+  bindings.addPostFrameCallback((_) => callback());
+
+  bindings.ensureVisualUpdate();
+
+  // switch (bindings.schedulerPhase) {
+  //   case SchedulerPhase.transientCallbacks:
+  //   case SchedulerPhase.midFrameMicrotasks:
+  //   case SchedulerPhase.persistentCallbacks:
+  //     bindings.addPostFrameCallback((_) => callback());
+  //     break;
+  //   case SchedulerPhase.postFrameCallbacks:
+  //   // Timer.run(callback);
+  //   // break;
+  //   case SchedulerPhase.idle:
+  //     // if (bindings.hasScheduledFrame) {
+  //     //   bindings.scheduleTask(callback, Priority.animation);
+  //     // } else {
+  //     //   Timer.run(callback);
+  //     // }
+  //     bindings.scheduleTask(callback, Priority.touch);
+  //     break;
+  // }
 }
